32cdec81e3e9387181ec8c336af335e51d83d53c
David Wenzel <codenull@gmail.com>
2017-01-06 12:13:44 -0800

    I got a boid moving!

--- js/scene.js
+++ js/scene.js
@@ -22,21 +22,22 @@ function init (numBoids) {
 
 	// var sky = MakeSkybox();
 	// scene.add(sky);
-var velocity = new THREE.Vector3(5);
-var pos = new THREE.Vector3(0,0,-20);
-var boid = new Boid(velocity,pos);
-boidList.push(boid);
-boid.mesh.position.set(0,0,-200);
-scene.add(boid.mesh);
+	var velocity = 5;
+	var pos = new THREE.Vector3(0,0,-200);
+
+// var boid = new Boid(velocity,pos);
+// boidList.push(boid);
+// boid.mesh.position.set(0,0,-200);
+// scene.add(boid.mesh);
 
 
-	// for (var i = 0; i < numBoids; i++) {
-	// 	var boid = new Boid();
-	// 	//add the new boid to the list
-	// 	boidList.push(boid)
-	// 	//add the mesh to the scene
-	// 	scene.add(boid.mesh);
-	// };
+	for (var i = 0; i < numBoids; i++) {
+		var boid = new Boid(velocity, pos);
+		//add the new boid to the list
+		boidList.push(boid)
+		//add the mesh to the scene
+		scene.add(boid.mesh);
+	};
 
 	scene.add(camera);
 
@@ -47,7 +48,6 @@ function animate() {
 	requestAnimationFrame(animate);
 	//perform animation functions over all boids + update camera position
 	for (boid in boidList) {
-		// boid.update(boidList);
 		boidList[boid].update(boidList);
 	}
 
@@ -56,39 +56,9 @@ function animate() {
 
 function updatePosition(neighbors) {
 //with credit for this algorithm to: http://www.vergenet.net/~conrad/boids/pseudocode.html
-	var rule1Vector = new THREE.Vector3();
-	var rule2Vector = new THREE.Vector3();
-	var rule3Vector = new THREE.Vector3();
-	var nearby = 1;
-	var distance;
-	var boid;
-	//TODO: consider speeding this up if needed with a sorted list?
-	for (foo in neighbors) {
-		boid = neighbors[foo];
-		distance = boid.mesh.position.distanceTo(this.mesh.position);
-	if (boid.mesh != this.mesh) {
-		if (distance < neighborhood) {
-			//collect nearby positions for averaging
-			rule1Vector += boid.position;
-			nearby += 1;
-			//check for rule 2:
-			if (distance < MINDISTANCE) {
-				rule2Vector -= (boid.mesh.position - this.mesh.position);
-			}
-			//apply rule 3:
-			rule3Vector += boid.velocity;
-		}
-	}
-}
-	rule1Vector = rule1Vector / nearby; //get the average
-	rule1Vector = (rule1Vector - self.position) / 100; // take %1 of the resulting vector
+	this.mesh.translateY(0.8);
+	this.mesh.rotation.z += .02;
 
-	rule3Vector = rule3Vector / nearby;
-	rule3Vector = (rule3Vector - this.velocity) / 8;
-
-	console.log(this.velocity.x);
-	this.velocity = this.velocity + rule1Vector + rule2Vector + rule3Vector;
-	this.mesh.position = this.position + this.velocity;
 	
 }
 
@@ -105,13 +75,15 @@ function boidGeom() {
 }
 
 function boidMat() {
-	return new THREE.MeshBasicMaterial( {color: 0xfff000} );
+	return new THREE.MeshBasicMaterial( {color: 0xff0000} );
 }
 
 function Boid(initVelocity, initPosition) {
 	this.mesh = new THREE.Mesh(boidGeom(), boidMat());
-	this.mesh.position = initPosition;
-	// this.mesh.rotation = initRotation;
+	this.mesh.position.set(0,0,-200);
+	// console.log(initPosition);
+	// this.mesh.position.set(initPosition);
+	
 	this.velocity = initVelocity;
 	this.position = getPosition;
 	this.heading = getHeading;

4e6947324c1cc02b04a79329e36a88c30124790e
David Wenzel <codenull@gmail.com>
2017-01-12 12:53:05 -0800

    Added dispersal key. (hold D to disperse, release to return flocking.

--- js/scene.js
+++ js/scene.js
@@ -2,6 +2,42 @@ var scene, camera, renderer;
 var MINDISTANCE = 10;
 var boidList = [];
 
+
+// Constants live here:
+
+var boidRange = 100;
+var maxSpeed = 2;
+var personalBubble = 20;
+var border = 200;
+
+// toggles for rules (as needed)
+var r1 = 1;
+
+function checkKey(e) {
+    e = e || window.event;
+    alert(e.keyCode);
+    console.log(e.keyCode);
+}
+
+function handleKeyUp(key) {
+	key = key || window.event;
+	if (key.keyCode == 68) {
+		console.log("combine");
+		r1 = 1;
+	}
+}
+
+function handleKeyDown(key) {
+	key = key || window.event;
+	if (key.keyCode == 68) {
+		console.log("disperse");
+		r1 = 0;
+	}
+}
+
+document.onkeydown = handleKeyDown;
+document.onkeyup = handleKeyUp;
+
 function init (numBoids) {
 	//set the scene
 
@@ -11,7 +47,7 @@ function init (numBoids) {
 	camera = new THREE.PerspectiveCamera(viewAngle, aspect, near, far);
 
 	//TODO: this should probably look at the COM for the whole flock
-	camera.position.set(0,0,200);
+	camera.position.set(0,0,500);
 	camera.lookAt(scene.position);
 	renderer = new THREE.WebGLRenderer( {
 		antialias: true,
@@ -22,7 +58,7 @@ function init (numBoids) {
 	var container = document.body;
 	container.appendChild(renderer.domElement);
 
-	var velocity = new THREE.Vector3(1,0,0);
+	var velocity = new THREE.Vector3(0,4,10);
 	var pos = new THREE.Vector3(0,0,0);
 
 	for (var i = 0; i < numBoids; i++) {
@@ -46,8 +82,21 @@ function animate() {
 	}
 
 	//update camera here.
+	// camera.lookAt(COM(boidList));
 
 	renderer.render(scene, camera);
+}
+
+function COM(neighbors) {
+	var vector = new THREE.Vector3();
+	var accum = 0;
+	for (i in neighbors) {
+		accum++;
+		vector.add(neighbors[i].mesh.position);
+	}
+	vector.divideScalar(accum);
+
+	return vector;
 }
 
 function ruleOne(neighbors, me) {
@@ -55,13 +104,13 @@ function ruleOne(neighbors, me) {
 	var accum = 0;
 	for (i in neighbors) {
 		if (me.mesh != neighbors[i].mesh && 
-			me.mesh.position.distanceTo(neighbors[i].mesh.position) < 200) {
+			me.mesh.position.distanceTo(neighbors[i].mesh.position) < boidRange) {
 			accum++;
 			vec1.add(neighbors[i].mesh.position);
 		}
 	}
 	vec1.divideScalar(accum);
-	return vec1.sub(me.mesh.position).divideScalar(200);
+	return vec1.sub(me.mesh.position).divideScalar(100);
 }
 
 function ruleTwo(neighbors, me) {
@@ -71,12 +120,12 @@ function ruleTwo(neighbors, me) {
 			if (me.mesh != neighbors[i].mesh) {
 				temp.subVectors(me.mesh.position, neighbors[i].mesh.position);
 
-				if (Math.abs(temp.length()) < 10) {
+				if (Math.abs(temp.length()) < personalBubble) {
 					pushBack.add(temp);
 				}
 			}
 	}
-	return pushBack;
+	return pushBack.divideScalar(1);
 }
 
 function ruleThree(neighbors, me) {
@@ -84,48 +133,49 @@ function ruleThree(neighbors, me) {
 	var accum = 0;
 	for (i in neighbors) {
 				if (me.mesh != neighbors[i].mesh && 
-			me.mesh.position.distanceTo(neighbors[i].mesh.position) < 200) {
+			me.mesh.position.distanceTo(neighbors[i].mesh.position) < boidRange) {
 					accum++;
 					velocity.add(neighbors[i].velocity);
 				}
 	}
 	velocity.divideScalar(accum);
 	velocity.sub(me.velocity);
-	return velocity.divideScalar(10);
+	return velocity.divideScalar(25);
 }
 
 function updatePosition(neighbors) {
+	// Three basic rules for emergent flocking behavior
 	var vec1 = ruleOne(neighbors, this);
 	var vec2 = ruleTwo(neighbors, this);
 	var vec3 = ruleThree(neighbors, this);
 
 
-	this.velocity.add(vec1);
+	this.velocity.add(vec1.multiplyScalar(r1));
 	this.velocity.add(vec2);
 	this.velocity.add(vec3);
 	this.velocity.add(bound(this));
 
-	this.velocity.clampLength(-1,1);
+	this.velocity.clampLength(-maxSpeed,maxSpeed);
 
 	
 	this.mesh.position.add(this.velocity);
-	this.mesh.position.setZ(0);
+	// this.mesh.position.setZ(0);
 	
 
 }
 
 function bound(boid) {
 	var distance = .5;
-	var xMin = -75;
-	var xMax = 75;
-	var yMin = -75;
-	var yMax = 75;
+	var xMin = -border;
+	var xMax = border;
+	var yMin = -border;
+	var yMax = border;
 	var vector = new THREE.Vector3();
 	if (boid.mesh.position.x < xMin) {
-		vector.x = distance;;
+		vector.x = distance;
 	}
 	else if (boid.mesh.position.x > xMax) {
-		vector.x = -distance;;
+		vector.x = -distance;
 	}
 	if (boid.mesh.position.y < yMin) {
 		vector.y = distance;
